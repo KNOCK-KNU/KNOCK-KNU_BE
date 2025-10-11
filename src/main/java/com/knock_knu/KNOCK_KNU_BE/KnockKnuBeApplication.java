e80ef924b5d30fe647e6e777b325a3f1dcffa020
milk-stone <ywshero@naver.com>
2025-10-11 21:15:13 +0900

    feat: Spring Boot 프로젝트 초기 설정

---
+++ src/main/java/com/knock_knu/KNOCK_KNU_BE/KnockKnuBeApplication.java
@@ -0,0 +1,13 @@
+package com.knock_knu.KNOCK_KNU_BE;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KnockKnuBeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(KnockKnuBeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/knock_knu/KNOCK_KNU_BE/KnockKnuBeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.knock_knu.KNOCK_KNU_BE;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KnockKnuBeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
