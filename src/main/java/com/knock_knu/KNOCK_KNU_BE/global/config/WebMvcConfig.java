08ff367b5d6fbf496dfd3c9f1c638c928a30c933
milk-stone <ywshero@naver.com>
2025-11-30 03:08:45 +0900

    feat: 프론트엔드 배포 서버 CORS 설정

--- src/main/java/com/knock_knu/KNOCK_KNU_BE/global/config/WebMvcConfig.java
+++ src/main/java/com/knock_knu/KNOCK_KNU_BE/global/config/WebMvcConfig.java
@@ -10,7 +10,7 @@ public class WebMvcConfig implements WebMvcConfigurer {
     @Override
     public void addCorsMappings(CorsRegistry registry) {
         registry.addMapping("/**")
-                .allowedOrigins("http://localhost:3000")
+                .allowedOrigins("http://localhost:3000", "https://knock-knu-fe.pages.dev/")
                 .allowedMethods("*")
                 .allowedHeaders("*")
                 .allowCredentials(true)
